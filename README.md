b173abf727fad0007ba0f6bad1737308920bf87e
David Akintola <akindayodavid22@gmail.com>
2024-01-05 17:12:02 +0100

    using dom to make rock paper scissors appear more user frendly

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rock-paper-scissors
+
+" a simple rock paper design using javascript along side google developer's tool
+s for consoling output"
--- script.js
+++ script.js
@@ -1,81 +1,104 @@
-console.log("hi")
-const option = ["rock", "paper", "scissors"]
+let playerScore = 0;
+let computerScore = 0;
+const container = document.querySelector('.container');
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+const result = document.querySelector('.result');
+const spanPlayer = document.querySelector(".plaScore");
+const spanComp= document.querySelector('.comScore');
+
+   
+//event
+rockButton.addEventListener('click', ()=>{
+    const playerSelection = "rock";
+    const ComputerSelection = getComputerChoice();
+    playRound(playerSelection, ComputerSelection);
+    scoreBoard(playerScore, computerScore)
+    
+    runScore(playerScore, computerScore);
+});
+
+paperButton.addEventListener('click', ()=>{
+    const playerSelection = "paper";
+    const ComputerSelection = getComputerChoice();
+    playRound(playerSelection, ComputerSelection);
+    scoreBoard(playerScore, computerScore)
+   
+    runScore(playerScore, computerScore);
+});
+
+scissorsButton.addEventListener('click', ()=>{
+    const playerSelection = "scissors";
+    const ComputerSelection = getComputerChoice();
+    playRound(playerSelection, ComputerSelection);
+    scoreBoard(playerScore, computerScore)
+    runScore(playerScore, computerScore);
+});
+
+
+//to check computer choice
 function getComputerChoice(){
+    const option = ["rock", "paper", "scissors"];
     const choice = option[Math.floor(Math.random() * option.length)];
+    console.log(choice);
     return choice;
-}getComputerChoice();
+};getComputerChoice();
 
-function checkWinner(playerSelection, ComputerSelection){
-    if(playerSelection == ComputerSelection){
-        return "Tie!";
-    } 
-    else if( (playerSelection =="rock" && ComputerSelection == "scissors") ||
-    (playerSelection == "scissors" && ComputerSelection =="paper") || 
-    (playerSelection == "paper" && ComputerSelection == "rock")){
-        return "player";
-    }
-    else {
-        return "Computer";
-    }
-}checkWinner();
+//score board
+function scoreBoard(playerScore, computerScore){
+    spanPlayer.innerText = `Player score: ${playerScore}`;
+    spanComp.innerText = `Computer score: ${computerScore}`;
+}scoreBoard()
 
+//to check who wins
 function playRound(playerSelection, ComputerSelection){
-    const result = checkWinner(playerSelection, ComputerSelection);
-    if(result == "Tie!"){
-        return "it's a Tie";
-    }
-    else if(result == "player"){
-        return `you win! ${playerSelection} beats ${ComputerSelection}`
-    }
-    else{
-        return `you lose! ${ComputerSelection} beats ${playerSelection}`
-    }
-}playRound()
-
-// const playerSelection ="rock";
-// const ComputerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, ComputerSelection));
+    if(playerSelection === ComputerSelection){
+        console.log('Tie');
+        const p = document.createElement('p');
+        p.textContent =("it's a Tie");
+        result.appendChild(p);
 
 
-function getPlayerChoice(){
-    let validationInput = false;
-    while(validationInput == false){
-        const choice = prompt("rock paper scissors");
-        if(choice == null ){
-            continue;
-        }
-        const choiceInLower = choice.toLowerCase();
-        if(option.includes(choiceInLower)){
-            validationInput = true
-            return choiceInLower;
-        }
     }
-}getPlayerChoice();
-
-
-function game(){
-    let scorePlayer = 0;
-    let ComputerPlayer = 0;
-    console.log("welcome") 
-    for(let i = 0; i < 5 ; i++){
-        const playerSelection= getPlayerChoice();
-        const ComputerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, ComputerSelection));
-    console.log("--------")
-    if(checkWinner(playerSelection, ComputerSelection) == "player"){
-        scorePlayer++;
-    }
-    else if(checkWinner(playerSelection, ComputerPlayer) == "Computer"){
-        ComputerPlayer++;
-    }
-}
-    console.log("game-over")
-    if(scorePlayer > ComputerPlayer){
-        console.log("player was the winner")
-    }else if(scorePlayer < ComputerPlayer){
-        console.log("computer was the winner")
+    else if( (playerSelection === "rock" && ComputerSelection === "scissors") ||
+            (playerSelection === "scissors" && ComputerSelection ==="paper") || 
+            (playerSelection === "paper" && ComputerSelection === "rock"))
+        {
+        console.log(`you win! ${playerSelection} beats ${ComputerSelection}`);
+        const p = document.createElement('p');
+        p.textContent =(`you win! ${playerSelection} beats ${ComputerSelection}`)
+        result.appendChild(p);
+        playerScore++;
     }else{
-        console.log("we have a tie");
+        console.log(`you lose! ${ComputerSelection} beats ${playerSelection}`);
+        const p = document.createElement('p');
+        p.textContent =(`you lose! ${ComputerSelection} beats ${playerSelection}`)
+        result.appendChild(p);
+        computerScore++;
+    };
+}playRound();
+
+
+
+//to check how many times of who wins
+
+function runScore(playerScore, computerScore){
+    console.log(playerScore, computerScore)
+    if(playerScore === 5){
+        const h2 = document.createElement('h2');
+        h2.classList.add('header-player');
+        h2.innerText = `You won ${playerScore} to ${computerScore}. Great job you beat Computer`;
+        result.append(h2)
+        playerScore++
     }
-}game()
+    if(computerScore === 5){
+        const h2 = document.createElement('h2');
+        h2.classList.add('header-computer');
+        h2.innerHTML = `You lost ${playerScore} to ${computerScore}. try again next time`;
+        console.log('hey');
+        result.append(h2)
+        computerScore++
+    }
+}runScore()
 
